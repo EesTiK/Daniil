8aaf9e25a87b1971e93ebe57c42da9016986779a
EesTiK <57655760+EesTiK@users.noreply.github.com>
2020-01-14 14:13:06 +0200

    Add files via upload

--- Helilaid.py
+++ Helilaid.py
@@ -1,72 +1,24 @@
-n, m = [int(i) for i in input().split()]
-a = [[int(j) for j in input().split()] for i in range(n)]
-best_i, best_j = 0, 0
-curr_max = a[0][0]
-for i in range(n):
-    for j in range(m):
-        if a[i][j] > curr_max:
-            curr_max = a[i][j]
-            best_i, best_j = i, j
-print(best_i, best_j)
-#2
-n = int(input())
-a = [['.'] * n for i in range(n)]
-for i in range(n):
-    a[i][i] = '*'
-    a[n // 2][i] = '*'
-    a[i][n // 2] = '*'
-    a[i][n - i - 1] = '*'
-for row in a:
-    print(' '.join(row))
-#3
-n, m = [int(i) for i in input().split()]
-a = []
-for i in range(n):
-    a.append([])
-    for j in range(m):
-        if (i + j) % 2 == 0:
-            a[i].append('.')
-        else:
-            a[i].append('*')
-for row in a:
-    print(' '.join(row))
-#4
-n = int(input())
-a = [[abs(i - j) for j in range(n)] for i in range(n)]
-for row in a:
-    print(' '.join([str(i) for i in row]))
-#5
-n = int(input())
-a = [[0] * n for i in range(n)]
-for i in range(n):
-    a[i][n - i - 1] = 1
-for i in range(n):
-    for j in range(n - i, n):
-        a[i][j] = 2
-for row in a:
-    for elem in row:
-        print(elem, end=' ')
-    print()
-#6
-def swap_columns(a, i, j):
-    for k in range(len(a)):
-        a[k][i], a[k][j] = a[k][j], a[k][i]
- 
-n, m = [int(i) for i in input().split()]
-a = [[int(j) for j in input().split()] for i in range(n)]
-i, j = [int(i) for i in input().split()]
-swap_columns(a, i, j)
-print('\n'.join([' '.join([str(i) for i in row]) for row in a]))
-#7
-n,m= map(int,input().split())
-nev = []
-res =[]
-for i in range(1,n*m+1):
-    nev.append(str(i))
-    if len (nev) == m:
-        res.append(nev)
-        nev = []
-for i,j in enumerate(res):
-    if i%2 !=0:
-        j = reversed(j)
-    print(*j)
+class Student():
+    def __init__(self ,name):
+        self.__name = name
+        self.__secname = ' '
+    def get_name(self):
+        print(self.__name)
+    def set_name(self, name):
+        self.__name = name
+    def get_secname(self):
+        print(self.__secname)
+    def set_secname(self, name):
+        self.__secname = name
+a = Student('lil')
+a.set_secname('lol')
+a.get_name()
+a.get_secname()
+a.set_name('lil')
+a.set_secname('lol')
+a.get_name()
+a.get_secname()
+b = Student('Danja')
+b.get_name()
+
+    
